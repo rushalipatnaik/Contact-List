1f8a3b3ef337bda70946af75ba19df4943cf8694
Rushali <rpatnaik78@gmail.com>
2022-01-17 22:05:15 +0530

    update

--- index.js
+++ index.js
@@ -7,14 +7,40 @@ const app = express();
 app.set('view engine', 'ejs'); 
 app.set('views', path.join(__dirname, 'views'));
 
+var contactList = [
+    {
+        name: "Rushali",
+        phone: "1234567890"
+    },
+    {
+        name: "Sai",
+        phone: "1234567890"
+    },
+    {
+        name: "Rush",
+        phone: "1234567890"
+    },
+    {
+        name: "Piggy",
+        phone: "1234567890"
+    }
+]
+
 app.get('/', function(req, res){
-   return res.render('home', {title: "Contact List"});
+   return res.render('home', {
+       title: "Contact List",
+       contact_list: contactList
+    });
 });
 
 app.get('/practice', function(req, res){
     return res.render('practice', {title: "Lets play with ejs"});
 });
 
+
+app.post('/create-contact', function(req,res){
+    return res.redirect('/practice');
+});
 
 
 app.listen(port, function(err){
