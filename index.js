caa05a199806d5b6ebf1ce78b470168f40181dda
Rushali <rpatnaik78@gmail.com>
2022-01-17 19:19:42 +0530

    update

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const path = require('path');
 const port = 8000;
 
 const app = express();
 
-app.get('/', function(req, res))
+app.set('view engine', 'ejs'); 
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', function(req, res){
+   return res.render('home');
+});
 
 
 
